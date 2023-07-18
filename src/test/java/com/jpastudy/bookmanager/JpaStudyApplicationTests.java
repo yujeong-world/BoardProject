d415663810fac389a364c8542744bf875593d82b
yujeong-world <kyjii082@gamil.com>
2023-07-18 17:32:39 +0900

    #1- 프로젝트 시작 및 테스트
    스프링웹, 롬복, jap, h2 db 의존성을 추가하여 프로젝트를 시작했다. HelloWorld 컨트롤러를 만들어보고 Junit사용하여 간단한 테스트를 진행했다.

---
+++ src/test/java/com/jpastudy/bookmanager/JpaStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpastudy.bookmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
