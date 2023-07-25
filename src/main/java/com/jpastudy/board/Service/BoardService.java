1c95763abc27531a6a342ccbd9b6305cffe8145b
yujeong-world <kyjii082@gamil.com>
2023-07-25 22:32:05 +0900

    #7 - 게시판 수정 기능 완료
    게시판 게시글 수정 기능 구현

--- src/main/java/com/jpastudy/board/Service/BoardService.java
+++ src/main/java/com/jpastudy/board/Service/BoardService.java
@@ -1,27 +1,20 @@
 package com.jpastudy.board.Service;
 
 import com.jpastudy.board.domain.Board;
-import com.jpastudy.board.domain.Comment;
 import com.jpastudy.board.domain.UserAccount;
 import com.jpastudy.board.dto.BoardDto;
 import com.jpastudy.board.dto.BoardWithCommentsDto;
-import com.jpastudy.board.dto.UserAccountDto;
 import com.jpastudy.board.repository.BoardsRepository;
-import com.jpastudy.board.repository.CommentRepository;
 import com.jpastudy.board.repository.UserAccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -45,9 +38,15 @@ public class BoardService {
     }*/
 
     //게시글 상세 정보 가지고 오기
-    public BoardWithCommentsDto getBoardById(Long boardId) throws ChangeSetPersister.NotFoundException {
+    public BoardWithCommentsDto getBoarWithCommentdById(Long boardId) throws ChangeSetPersister.NotFoundException {
         Board board = boardsRepository.findById(boardId).orElseThrow(()->new ChangeSetPersister.NotFoundException());
         return BoardWithCommentsDto.from(board);
+    }
+
+    //게시글 수정 페이지에 원본 글 삽입하기
+    public BoardDto getBoardModifyById(Long boardId) throws ChangeSetPersister.NotFoundException {
+        Board board = boardsRepository.findById(boardId).orElseThrow(()->new ChangeSetPersister.NotFoundException());
+        return BoardDto.from(board);
     }
 
     //게시글 검색 기능
@@ -75,6 +74,20 @@ public class BoardService {
     public void boardDelete(Long boardId){
         boardsRepository.deleteById(boardId);
     }
+
+    //게시판 글 수정
+    @Transactional
+    public BoardDto boardModify(Long boardId,String title, String content) throws ChangeSetPersister.NotFoundException {
+        Board board =boardsRepository.findById(boardId).orElseThrow(()->new ChangeSetPersister.NotFoundException());
+/*        board.setContent(board.getContent());
+        board.setTitle(board.getTitle());*/
+        board.setTitle(title);
+        board.setContent(content);
+
+        return BoardDto.from(board);
+    }
+
+
 
 
 
